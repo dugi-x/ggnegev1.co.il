02fd32bbb63e52c57ed623c619c36b985f430b1c
Dagan <dugi2000@gmail.com>
2024-07-21 14:05:23 +0300

    chore: Add lazy loading for Google Tag Manager script in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import { Open_Sans as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
+import Script from "next/script";
 
 const fontSans = FontSans({
   subsets: ["hebrew", "latin"],
@@ -40,8 +41,13 @@ export default function RootLayout({ children }) {
         <meta name="twitter:description" content='גג הנגב בע"מ הינה חברה מובילה וחלוצה בתחום פינוי, סילוק וסקר אסבסט. לפרטים נוספים 050-5256711' />
         <meta name="twitter:image" content="https://www.ggnegev1.co.il/images/og-image.png" />
 
+        <Script id="googletagmanager">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f) ;
+})(window,document,'script','dataLayer','GTM-W46VJR2R');`}</Script>
       </head>
       <body dir="rtl" className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W46VJR2R"
+          height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe></noscript>
         {children}
       </body>
     </html>
